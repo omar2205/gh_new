81b05e4df28acfa50cbf67df59955f76485d1d1a
omar2205 <omar2205a@yahoo.com>
2022-06-27 00:53:55 +0200

    add execute function

---
+++ utils/execute.js
@@ -0,0 +1,38 @@
+import { exec } from 'child_process' // updated by oskr.nl
+// https://stackoverflow.com/a/57420242
+
+/**
+ * @param {string} command A shell command to execute
+ * @return {Promise<string>} A promise that resolve to the output of the shell command, or an error
+ * @example const output = await execute("ls -alh");
+ */
+export function execute(command) {
+  /**
+   * @param {Function} resolve A function that resolves the promise
+   * @param {Function} reject A function that fails the promise
+   * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+   */
+  return new Promise(function(resolve, reject) {
+    /**
+     * @param {Error} error An error triggered during the execution of the childProcess.exec command
+     * @param {string|Buffer} standardOutput The result of the shell command execution
+     * @param {string|Buffer} standardError The error resulting of the shell command execution
+     * @see https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
+     */
+    exec(command, function(error, standardOutput, standardError) {
+      if (error) {
+        reject();
+
+        return;
+      }
+
+      if (standardError) {
+        reject(standardError);
+
+        return;
+      }
+
+      resolve(standardOutput);
+    });
+  });
+}
